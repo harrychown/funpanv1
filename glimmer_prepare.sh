080035d4bbf982c46dd17b2c51050c2306c3be15
harrychown <harrychown98@gmail.com>
2022-07-06 13:20:10 +0100

    Current position: prediction, demo scripts for debugging

--- glimmer_prepare.sh
+++ glimmer_prepare.sh
@@ -6,9 +6,11 @@ cd /data/global/prediction/glimmerHMM
 : <<'END'
 # Remove description tags from FASTA
 sed 's/ |.*//g' FungiDB-58_AfumigatusAf293_AnnotatedCDSs.fasta > Af293.cds.fasta
+grep ">" Af293.cds.fasta | sed 's/>//g' > cds_in_fasta.txt
 # Extract exon names and information
-grep -P "\texon\t" FungiDB-58_AfumigatusAf293.gff | sed 's/.*Parent=//g'| cut -d";" -f1 > exon.names.tmp
-grep -P "\texon\t" FungiDB-58_AfumigatusAf293.gff | cut -d$'\t' -f3-5 > exon.info.tmp
+grep -f cds_in_fasta.txt FungiDB-58_AfumigatusAf293.gff > Af293.matched.gff
+grep -P "\texon\t" Af293.matched.gff | sed 's/.*Parent=//g'| cut -d";" -f1 > exon.names.tmp
+grep -P "\texon\t" Af293.matched.gff | cut -d$'\t' -f3-5 > exon.info.tmp
 
 # Combine the ID and exons
 paste exon.names.tmp exon.info.tmp | column -s $'\t' -t > exon.combined.tmp
@@ -24,6 +26,16 @@ END
 cat exon.combined.tmp | awk '{print $1}' | cat -n |  sort -uk2 | sort -n | cut -f2- > exon.combined.order.tmp
 
 samtools faidx Af293.cds.fasta $(cat exon.combined.order.tmp) > Af293.ordered.fasta
+
+
+sed 's/exon //g' exon.combined.tmp > exon.file
+
+/home/harry/GlimmerHMM-3.0.4/GlimmerHMM/train/trainGlimmerHMM Af293.ordered.fasta exon.file
+
+/home/harry/GlimmerHMM-3.0.4/GlimmerHMM/train/trainGlimmerHMM Af293.ordered.fasta exon.file
+/home/harry/GlimmerHMM-3.0.4/GlimmerHMM/sources/glimmerhmm Af293.fna -d train_glim_4_7 -g 
+
+
 #grep "protein_coding_gene" FungiDB-58_AfumigatusAf293.gff | sed 's/.*ID=//g' | cut -d";" -f1 | wc -l
 #grep -P "\texon\t" FungiDB-58_AfumigatusAf293.gff > exon.gff
 
